b080515187ec3d1d951928f64677b99333cbea94
Frank LaNasa <flanasa@adharmonics.com>
2017-05-11 00:01:43 -0400

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (newMessage) => {
     console.log('new message', newMessage);
     io.emit('newMessage', {
